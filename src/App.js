5c44c1775aca163cf49fdc93819abe8992fa93da
Mate Krezic <matekrezic@gmail.com>
2021-02-16 14:52:55 +0100

    Fetched and displayed data without styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
-import locations from './database/locations.json';
-import React, { useState } from 'react';
+import "./App.css";
+import locations from "./database/locations.json";
+import React, { useState } from "react";
+import SingleLocation from "./components/SingleLocation";
 
 function App() {
-  const [selectedLocation, setSelectedLocation] = useState(null)
+  const [selectedLocation, setSelectedLocation] = useState(null);
   return (
     <div className="App">
-      {!selectedLocation ?
-        locations.map(location =>  <div onClick={()=>setSelectedLocation(location)}>{location.name}</div>)
-       : <div>
-          ...INSERT SELECTED LOCATION DATA HERE...
-        </div>}
-      {!!selectedLocation &&  <div onClick={()=>setSelectedLocation(null)}>{"<- BACK"}</div>}
+      {!selectedLocation ? (
+        locations.map((location) => (
+          <div onClick={() => setSelectedLocation(location)}>
+            <SingleLocation location={location} />
+          </div>
+        ))
+      ) : (
+        <div>...INSERT SELECTED LOCATION DATA HERE...</div>
+      )}
+      {!!selectedLocation && (
+        <div onClick={() => setSelectedLocation(null)}>{"<- BACK"}</div>
+      )}
     </div>
   );
 }
--- src/components/SingleLocation.js
+++ src/components/SingleLocation.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const SingleLocation = () => {
-  return <div></div>;
+const SingleLocation = ({ location }) => {
+  const { category, name, address, imageUrl } = location;
+  return (
+    <div>
+      <img src={imageUrl} alt={name} />
+      <h4>{category}</h4>
+      <h3>{name}</h3>
+      <p>{address}</p>
+    </div>
+  );
 };
 
 export default SingleLocation;
