a6b9ae81f34ed655118906c85e9abdda57f85087
Antonija Oslovčan <oslovcana22@gmail.com>
2021-02-17 20:18:00 +0100

    Location Details component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import './App.css';
-import locations from './database/locations.json';
-import React, { useState } from 'react';
-import LocationsList from './components/LocationsList';
-import Navigation from './components/Navigation';
+import "./App.css";
+import locations from "./database/locations.json";
+import React, { useState } from "react";
+import LocationsList from "./components/LocationsList";
+import Navigation from "./components/Navigation";
+import LocationDetails from "./components/LocationDetails";
 
 function App() {
-  const [selectedLocation, setSelectedLocation] = useState(null)
-
-  /*{/*<div onClick={()=>setSelectedLocation(location)}>{location.name}</div>*/
+  const [selectedLocation, setSelectedLocation] = useState(null);
   return (
+ 
    
     <div className="App">
-       <Navigation/>
+    <Navigation />
       <div className="container">
         <div className="row">
-      {!selectedLocation ?
-        locations.map(location => <LocationsList data={location} modal={() => setSelectedLocation(location)}/> )
-       : <div>
-          ...INSERT SELECTED LOCATION DATA HERE...
-        </div>}
-      {!!selectedLocation &&  <div onClick={()=>setSelectedLocation(null)}>{"<- BACK"}</div>}
+          {!selectedLocation ? (
+            locations.map((location) => (
+              <LocationsList
+                data={location}
+                modal={() => setSelectedLocation(location)}
+              />
+            ))
+          ) : (
+            <LocationDetails dataLocation={selectedLocation} />
+          )}
+       
+        </div>
       </div>
-      </div>
+      {!!selectedLocation && (
+        <div className="btn-back" onClick={() => setSelectedLocation(null)}>
+          {"BACK"}
+        </div>
+      )}
     </div>
+    
   );
 }
 
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -4,14 +4,14 @@ import '../style/LocationList.css';
 function LocationsList(props) {
   return (
     <div className="col-sm-3 " onClick={props.modal}>
-        <div className="location-item">
+      <div className="location-item">
         <img className="col-sm-12 location-item-image" src={props.data.imageUrl} />
         <div className="item-content">
-        <h2 className="location-item-title">{props.data.name}</h2>
-        <p className="location-item-description">{props.data.description}</p>
+          <h2 className="location-item-title">{props.data.name}</h2>
+          <p className="location-item-description">{props.data.description}</p>
         </div>
-      
-        </div>
+
+      </div>
     </div>
   );
 }
