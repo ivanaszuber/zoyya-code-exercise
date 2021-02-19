f5d2a51e15d29d3688f7b75b500e5bcfea6dd954
Denis <15inchdesign@users.noreply.github.com>
2021-02-19 05:21:24 +0100

    updates

--- src/components/locationsList.js
+++ src/components/locationsList.js
@@ -3,29 +3,29 @@ import locations from "../database/locations.json";
 
 class LocationsList extends Component {
   render() {
-    return locations.map((location) => (
-      <div className='card' key={location.id}>
+    return locations.map((location, index) => (
+      <div className='card' key={index}>
         <span className='card-header'>
           <img src={location.imageUrl} alt={location.name} />
           <span className='card-title'>
             <h3>{location.name}</h3>
           </span>
         </span>
-        <span className="card-summary">
+        <span className='card-summary'>
           <p>{location.category}</p>
         </span>
-        <span className="card-summary">
+        <span className='card-summary'>
           <p>{location.address}</p>
         </span>
-        <span className="card-summary">
+        <span className='card-summary'>
           <p>{location.city}</p>
         </span>
-        <span className="card-meta">
-          See Location
-        </span>
+        <span className='card-meta'>See Location</span>
       </div>
     ));
   }
 }
 
 export default LocationsList;
+
+
--- src/components/locationtDetail.js
+++ src/components/locationtDetail.js
@@ -3,7 +3,7 @@ import locations from "../database/locations.json";
 
 class LocationDetail extends Component {
     render() {
-      return locations.map((location) => (<div key={location.id}>{location.name}</div>));
+      return locations.map((location) => <div key={location.name}>{location.name}</div>);
     }
 }
 
